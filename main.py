49c1fcb0f720bb33582bfa42b2d6d3045c13bfe8
Sanguk Lee <dltkddnr04@icloud.com>
2024-09-09 20:19:49 +0900

    캘린더 alias_code로 접근하게 변경

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import json
 import requests
 import datetime
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, Response
 
 with open("config.json", "r") as f:
     config = json.load(f)
     ID = config["id"]
     PW = config["pw"]
-    CALENDER_ID = config["calendar_id"]
+
+session_id = None
 
 def signup(id=ID, pw=PW):
+    global session_id
+
     url = "https://timetreeapp.com/api/v1/auth/email/signin"
     headers = {
         "Content-Type": "application/json",
@@ -23,11 +26,41 @@ def signup(id=ID, pw=PW):
     }
     req = requests.put(url, headers=headers, data=json.dumps(data))
     try:
-        return req.cookies['_session_id']
+        session_id = req.cookies['_session_id']
+        return session_id
     except:
         print(req.text)
         print(req.status_code)
         return None
+    
+def get_calender_list(session_id):
+    url = "https://timetreeapp.com/api/batch"
+    data = {
+        "ops": [{
+            "method": "get",
+            "url": "/v1/calendars",
+            "params": {
+                "since": 0
+            }
+        }],
+        "sequential": True
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "X-TimeTreeA": "web/2.1.0/ko",
+    }
+    cookies = {
+        "_session_id": session_id,
+    }
+    req = requests.post(url, headers=headers, cookies=cookies, data=json.dumps(data))
+    if req.status_code == 200:
+        calender_list_data = req.json()['results'][0]['body']['calendars']
+        calender_list = {}
+        for calender in calender_list_data:
+            calender_list[calender['alias_code']] = [calender['id'], calender['name']]
+        return calender_list
+    else:
+        get_calender_list(signup())
     
 def get_calendar(calendar_id, session_id):
     url = f"https://timetreeapp.com/api/v1/calendar/{calendar_id}/events/sync"
@@ -43,9 +76,9 @@ def get_calendar(calendar_id, session_id):
     else:
         get_calendar(calendar_id, signup())
 
-def convert_to_ics(calendar):
-    ics_data = "BEGIN:VCALENDAR\nVERSION:2.0\nX-WR-CALNAME:포세이돈\nX-WR-TIMEZONE:Asia/Seoul\n"
-    for event in calendar['events']:
+def convert_to_ics(calendar_data, calender_name="TimeTree"):
+    ics_data = f"BEGIN:VCALENDAR\nVERSION:2.0\nX-WR-CALNAME:{calender_name}\nX-WR-TIMEZONE:Asia/Seoul\n"
+    for event in calendar_data['events']:
         title = event['title']
         start_at = datetime.datetime.fromtimestamp(event['start_at'] / 1000).strftime("%Y%m%dT%H%M%S")
         end_at = datetime.datetime.fromtimestamp(event['end_at'] / 1000).strftime("%Y%m%dT%H%M%S")
@@ -54,10 +87,28 @@ def convert_to_ics(calendar):
     ics_data += "END:VCALENDAR"
     return ics_data
 
+session_id = signup()
 app = FastAPI()
 
-@app.get("/calendar.ics")
-def read_ics():
-    calendar = get_calendar(CALENDER_ID, signup())
-    ics_data = convert_to_ics(calendar)
-    return StreamingResponse(content=ics_data, media_type="text/calendar")
+@app.get("/")
+def read_root():
+    return "Hello, World!"
+
+@app.get("/list")
+def read_list():
+    # return calender_list
+    calender_list = get_calender_list(session_id)
+    # remove id from calender_list
+    for key in calender_list:
+        calender_list[key] = calender_list[key][1]
+    return calender_list
+
+@app.get("/{ALIAS_CODE}.ics")
+def read_ics(ALIAS_CODE: str):
+    calender_list = get_calender_list(session_id)
+    if ALIAS_CODE in calender_list:
+        calendar = get_calendar(calender_list[ALIAS_CODE][0], session_id)
+        ics_data = convert_to_ics(calendar, calender_list[ALIAS_CODE][1])
+        return StreamingResponse(content=ics_data, media_type="text/calendar")
+    else:
+        return Response(content="No such calendar", media_type="text/plain", status_code=404)
